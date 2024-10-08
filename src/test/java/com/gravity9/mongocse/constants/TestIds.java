dd254667e250f18265e676a4767697fce721ba41
karolkujawski <karol.kujawski@gravity9.com>
2024-10-08 14:44:00 +0200

    Additional tests for ChangeStreams and ConfigManager verification - 2 failing need consultation

--- src/test/java/com/gravity9/mongocse/constants/TestIds.java
+++ src/test/java/com/gravity9/mongocse/constants/TestIds.java
@@ -2,9 +2,11 @@ package com.gravity9.mongocse.constants;
 
 public class TestIds {
 
-	public static final String MOD_0_ID = "652e9fdc597d12ddbf7380e7";
+    public static final String MOD_0_ID = "652e9fdc597d12ddbf7380e7";
 
-	public static final String MOD_1_ID = "652e9f6fcd6b9a316b067843";
+    public static final String MOD_1_ID = "652e9f6fcd6b9a316b067843";
 
-	public static final String MOD_2_ID = "652ea13969adc932efb550d4";
+    public static final String MOD_2_ID = "652ea13969adc932efb550d4";
+
+    public static final String MOD_3_ID = "000e9fdc597d12ddbf738000";
 }
